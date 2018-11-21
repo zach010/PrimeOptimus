2252fc1d9666912bc3ec4bca941b779014358408
Zach <34301452+zach010@users.noreply.github.com>
2018-11-21 02:46:44 -0800

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -66,7 +66,6 @@ def segregate(num, precision):
 def initialize(numb_seg, cores, number, precision):
     sync = Barrier(cores)
     q_list = [(multiprocessing.Queue()) for _ in range(cores)]
-    t_list = [multiprocessing.Queue()]
     n_list = [[0 for _ in range(3)] for _ in range(cores)]
     mpf_number = mp.mpf(number)
     for s in range(cores):
